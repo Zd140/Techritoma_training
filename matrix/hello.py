1805124510a17eed028e05be190343bd0a25fe94
Zd140 <delaneyzingwa@gmail.com>
2022-09-17 00:23:47 +0100

    changes

---
+++ matrix/hello.py
@@ -0,0 +1,15 @@
+#3 x 3 matrix
+from unittest import result
+
+
+X = [[12,7,3], [4,5,6], [7,8,9]]
+
+#3 x 4 matrix
+Y = [[5,8,1,2], [6,7,3,0], [4,5,9,1]]
+
+result = [[sum(a * b for a, b in zip(X_row, Y_col)) for Y_col in zip(*Y)] 
+                                                        for X_row in X]
+                                
+print(f"the result is:")
+for r in result:
+    print(r)

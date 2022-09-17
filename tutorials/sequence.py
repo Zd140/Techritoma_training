d3402cfcac43b55c4eeca6a2a755aa9250bb9da2
Zd140 <delaneyzingwa@gmail.com>
2022-09-17 01:16:25 +0100

    second commit

---
+++ tutorials/sequence.py
@@ -0,0 +1,42 @@
+#list tupples strings dictionaries and sets
+"""1 list are mutable sequences that is their values can be changes at any point in time.
+- indexing or licing is is possible with these objects.
+-going to be printing their types.
+-list contains vaalues of different data types
+-written in square bracees.
+
+2 tuples are immutable sequences ie opposite to list
+- written in  brakets
+
+3 dictionaries key value pairs.
+-written in curly braces
+can be muted
+
+4 set created curly braces
+-no key value pairs
+-unique vales
+
+5indexing accessing particular values
+"""
+#list
+li = [1,"man",1.0, "mangoes", "banana"]
+print(li)
+print(type(li))
+print()
+
+#tuple
+tup = ("BMW", "FORD", "jaguar", "Tesla")
+print(tup)
+print(type(tup))
+print()
+
+#dictionaries
+employee = {"name" : "john", "age" : 20, "school" : "UB", "sex" : "Male",}
+print(employee)
+print(type(employee))
+print()
+
+#set
+month = {"jan", "feb", "april", "may"}
+print(month)
+print(type(month))
